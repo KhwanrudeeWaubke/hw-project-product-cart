4bcca2e748779aa5e8efce1181d9b838bdaf0626
khwanrudeewaubke <nan_khwanrudee@hotmail.com>
2025-07-30 13:36:06 +0700

    init

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hw-project-product-cart/',
   plugins: [react(),tailwindcss(),],
 })
